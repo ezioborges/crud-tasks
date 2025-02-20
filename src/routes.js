811c61a2463fa71243038a7d24982951f9e1a35c
ezioborges <eziopborges@gmail.com>
2025-02-20 14:05:49 -0300

    method DELETE created

--- src/routes.js
+++ src/routes.js
@@ -45,5 +45,16 @@ export const routes = [
 
             return res.writeHead(204).end()
         }
+    },
+    {
+        method: 'DELETE',
+        url: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+
+            database.delete('tasks', id)
+
+            return res.writeHead(200).end()
+        } 
     }
 ]
